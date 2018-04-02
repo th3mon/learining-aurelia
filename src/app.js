5dfe7c1e3967f16d1d60999bfd34489ec7cc65e3
th3mon <p.szelenberger@gmail.com>
2018-04-03 01:23:54 +0200

    Router configuration.

--- src/app.js
+++ src/app.js
@@ -2,4 +2,14 @@ export class App {
   constructor() {
     this.message = 'Hello World!';
   }
+
+  configureRouter(config, router) {
+    this.router = router;
+    config.title = 'Learning Aurelia';
+    config.map([
+      { route: '', redirect: 'contacts' },
+      { route: 'contacts', name: 'contacts', moduleId: 'contact-list', nav: true, title: 'Contacts' },
+      { route: 'contacts/:id', name: 'contact-details', moduleId: 'contact-details' }
+    ]);
+  }
 }
