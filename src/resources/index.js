cf5e98aeab42cd82e88022429edf2069345433b1
th3mon <p.szelenberger@gmail.com>
2018-05-31 23:05:35 +0200

    FilterBy value converter.

--- src/resources/index.js
+++ src/resources/index.js
@@ -1,6 +1,7 @@
 export function configure(config) {
   config.globalResources([
     './value-converters/order-by',
-    './value-converters/group-by'
+    './value-converters/group-by',
+    './value-converters/filter-by'
   ]);
 }
